5f25d1fbcd7d8fbe281174effde1507832309146
Ali Karimi <ajkarimi7@gmail.com>
2019-08-14 22:50:53 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,12 +3,12 @@
 def valid_move?(board, index)
   
   if index.between?(1,9)
-    if !position_taken(board, index)
+    if !position_taken?(board, index)
       true
     end 
   end 
   
-  index.between?(0,8) && !position_taken(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
 
 end 
 
